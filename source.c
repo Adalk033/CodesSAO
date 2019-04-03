595e12da01d7fbae132afb5027458398c373760c
Adalk033 <37385524+Adalk033@users.noreply.github.com>
2019-04-03 10:42:31 -0600

    Add files via upload
    1.- prototipos.h
    2.- pila.h
    3.- profundidad.c
    4.- posfijo.c
    5.- evaluar.c
    6.- source.c

--- source.c
+++ source.c
@@ -1,92 +1,29 @@
 #include <malloc.h>		//Manejo de memoria dinamica
 #include <stdio.h>		//Input y Output
 #include <stdlib.h>		//Rutinas de utileria
-
-struct nodo
-{
-	char info;			//info - dato a preservar
-	struct nodo *sig;	//sig - Siguiente nodo de informacion
-};
-
-struct nodo *raiz = NULL;
-
-void push (char dato)
-{
-	struct nodo *nuevo;
-	if (raiz == NULL)
-	{
-
-		//Lo que hace es convertir la direccion de memoria
-		nuevo = (struct nodo *) malloc( sizeof(struct nodo) );
-		raiz = nuevo;
-		nuevo -> info = dato;
-		nuevo -> sig = NULL;
-	}
-	else
-	{
-		struct nodo *ultimo = raiz;
-		nuevo = (struct nodo *) malloc( sizeof(struct nodo) );
-		nuevo -> info = dato;
-		nuevo -> sig = raiz;
-		raiz = nuevo;
-	}
-}
-
-char pop()
-{
-	char temp;
-	if ( isEmpty() == 1)
-		printf("No se puede, la pila esta vacia\n");
-	else
-	{
-		temp = raiz -> info;
-		raiz = raiz -> sig;
-	}
-	return temp;
-}
-
-int isEmpty()
-{
-	if (raiz == NULL)
-		return 1;
-	else 
-		return 0;
-}
-
-char stackTop()
-{
-	if ( isEmpty() == 1)
-	{
-		printf("No hay datos\n");
-		return '-';
-	}
-	else
-		return raiz ->info;
-	
-}
-
-void imprime()
-{
-	struct nodo *recorre = raiz;
-	while (recorre != NULL)
-	{
-		printf("Dato = %c\n", recorre -> info);
-		recorre = recorre->sig;
-	}
-	printf("\n\n");
-}
+#include "prototipos.h"
+#include "pila.h"
 
 int main()
 {
-	push('L');
-	push('A');
-	push('D');
-	push('A');
-	imprime();
-	printf("Sale el dato: %c\n", pop());
-	imprime();
-	printf("El dato de arriba es: %c\n", stackTop());
-	imprime();
-	system("pause");
+	double resultado=0;
+	char express[100];
+	int value=0;
+
+	//Pedir expresion
+	fflush(stdin);
+	gets(express);
+
+	value = profundidad(express);
+	if (value == 1)
+	{
+		printf("Esta bien la expresion\n");
+		postfijo(express);
+		resultado = evaluar(express);
+		printf("%2.1f\n", resultado);
+	}
+	else
+		printf("Esta mal y no se puede hacer la expresion\n");
+	system("PAUSE");
 	return 0;
 }
